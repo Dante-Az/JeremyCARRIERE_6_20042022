9bbacf3bbdda73cb230158c7bbe0318452da4f1b
Dante-Az <jeremyo.carriere@gmail.com>
2022-04-20 16:56:57 +0200

    setup mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,16 @@
+// Importation d'express
 const express = require('express');
+// Importation de mongoose
+const mongoose = require('mongoose');
 
+
+mongoose.connect('mongodb+srv://JeremyC:Gototheparadise6@cluster0.vky2v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+//Appel de la méthode express
 const app = express();
 
 
