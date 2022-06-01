7f4c651826ac0a2e451312ff74b7684880cf0fe8
Dante-Az <jeremyo.carriere@gmail.com>
2022-06-01 12:27:43 +0200

    conversion async

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -5,8 +5,9 @@ const fs = require('fs');
 
 
 // Pour ajouter une sauce en POST
-exports.createSauce= (req, res, next) => {
+exports.createSauce = async (req, res, next) => {
   // On récupére les champs dans le corps de la requête
+  try {
   const sauceObject = JSON.parse(req.body.sauce);
 
   // On crée ne nouvelle instance de Sauce
@@ -16,13 +17,17 @@ exports.createSauce= (req, res, next) => {
     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
   });
   // On enregistre l'objet dans la BDD avec une promesse
-  sauce.save()
-  .then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
-  .catch(error => res.status(400).json({ error }));
+   const saveSauce = await sauce.save({}); 
+  res.status(201).json(saveSauce);
+  
+  } catch (error) {
+    res.status(404).json({error: error});
+  }
 };
 
 // Pour modifier une sauce en PUT avec la méthode updateOne
-exports.modifySauce = (req, res, next) => {
+exports.modifySauce = async (req, res, next) => {
+  try {
     // On vérifie si le fichier image existe ou non
     const sauceObject = req.file ?
       {
@@ -30,37 +35,42 @@ exports.modifySauce = (req, res, next) => {
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
       } : { ...req.body };
     // On utilise le paramètre id de la requête pour trouver la Sauce et la modifier avec le meme _id qu'avant, sans en générer un nouveau 
-    Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
-      .catch(error => res.status(400).json({ error
-    }));
+    const updateSauce = await Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+      res.status(200).json(updateSauce);
+  } catch (error) {
+    res.status(404).json({error: error}); 
+  }  
 };
 
  // Pour supprimer une sauce avec DELETE seulement si sauce.userId == auteur de la requête
- exports.deleteSauce = (req, res, next) => {
+ exports.deleteSauce = async (req, res, next) => {
    // Pour trouver la sauce 
-  Sauce.findOne({ _id: req.params.id })
-     .then((sauce) => {
-       if (!sauce) {
-         res.status(404).json({ error: new Error("Cette sauce n'existe pas !")});
-       }
-       // On met l'identifiant mis dans objet requête utilsé pour le comparer avec le userId de la sauce
-       if (sauce.userId !== req.auth.userId) {
-         res.status(400).json({ error: new Error('Requête non autorisée !')});
-       }
-       return sauce;
-     })
-     .then(sauce => {
-       // On récupère le nom de fichier
-       const filename = sauce.imageUrl.split('/images/')[1];
-       // On supprime le fichier puis  on effectue le callback qui le supprime de la BDD
-       fs.unlink(`images/${filename}`, () => {
-         Sauce.deleteOne({ _id: req.params.id })
-          .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
-           .catch(error => res.status(400).json({ error }));
-       });
-     })
-     .catch(error => res.status(500).json({ error }));
+   try {
+     const findOne = await Sauce.findOne({ _id: req.params.id })
+
+      if (!findOne) {
+        res.status(404).json({ error: new Error("Cette sauce n'existe pas !")});
+      }
+      // On met l'identifiant mis dans objet requête utilsé pour le comparer avec le userId de la sauce
+      if (findOne.userId !== req.auth.userId) {
+        res.status(400).json({ error: new Error('Requête non autorisée !')});
+      }
+      // On récupère le nom de fichier
+      
+        const filename = findOne.imageUrl.split('/images/')[1];
+      // On supprime le fichier puis  on effectue le callback qui le supprime de la BDD
+      fs.unlink(`images/${filename}` ,async () => {
+        try {
+        const delSauce = await Sauce.deleteOne({ _id: req.params.id });
+         res.status(200).json(delSauce)
+      }
+      catch (error) {
+        res.status(404).json({error: error}); 
+      }
+      });    
+   } catch (error) {
+    res.status(500).json({error: error}); 
+  }   
  };
 
 // Pour récupérer une seule sauce avec la méthode findOne
