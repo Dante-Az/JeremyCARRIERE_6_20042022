a6b2dbc68768a1fb663f827e1426d1c227a289a6
Dante-Az <jeremyo.carriere@gmail.com>
2022-06-01 12:29:13 +0200

    sauce routes

---
+++ backend/routes/sauce.js
@@ -0,0 +1,29 @@
+// Importation d'express
+const express = require('express');
+// Création d'un router 
+const router = express.Router();
+
+// Importation du middleware d'authentification
+const auth = require('../middleware/auth');
+// Importation de multer
+const multer = require('../middleware/multer-config');
+
+
+// Importation du controller des sauces
+const sauceCtrl = require('../controllers/sauce');
+// Importation du controller des likes/dislikes
+const likeCtrl = require('../controllers/likes');
+
+
+// On enregistre les routes dans le router et  on applique des fonctions du controller 
+// auth applique authentification à toutes les routes (à placer en 1er)
+// multer applique l'ajout de fichier aux routes post et put
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.get('/', auth, sauceCtrl.getAllSauces);
+router.post('/:id/like', auth, likeCtrl.likeSauce);
+
+// export du router
+module.exports = router;
